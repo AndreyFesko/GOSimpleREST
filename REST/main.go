166ae664d97e073a37af07573e218301349038c0
Andrey Fesko <andrei.f@core-tech.ru>
2019-05-23 15:56:14 +0300

    Implemented cash accounts, transactions. WriteOff, Refill, Transfer.

--- REST/main.go
+++ REST/main.go
@@ -7,11 +7,10 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	//"sync"
 
-	//"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"gopkg.in/guregu/null.v3"
 )
 
 type Employee struct {
@@ -22,9 +21,17 @@ type Employee struct {
 }
 
 type Account struct {
-	ID      int `json:"id"`
-	ID_User int `json:"id_user"`
-	Value   int `json:"value"`
+	ID     int `json:"id"`
+	UserID int `json:"user_id"`
+	Value  int `json:"value"`
+}
+
+type Transaction struct {
+	ID                    int      `json:"id"`
+	Type                  string   `json:"type"`
+	AccountForOperationID int      `json:"acc_for_operation"`
+	RecievedIDAccount     null.Int `json:"recieved_id"`
+	Value                 int      `json:"value"`
 }
 
 const (
@@ -35,27 +42,36 @@ const (
 	dbname   = "dbank"
 )
 
-func dbConnect() (db *sql.DB) {
+var db *sql.DB
+
+func init() {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	var err error
+	db, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err)
 	}
-	return db
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 	r := mux.NewRouter()
-	s := r.PathPrefix("/users").Subrouter()
+	s := r.PathPrefix("/").Subrouter()
 
-	s.HandleFunc("/", ListUsers).Methods(http.MethodGet)
-	s.HandleFunc("/", CreateUser).Methods(http.MethodPost)
+	s.HandleFunc("/users", ListUsers).Methods(http.MethodGet)
+	s.HandleFunc("/users", CreateUser).Methods(http.MethodPost)
 
-	s.HandleFunc("/{id}", ReadUser).Methods(http.MethodGet)
-	s.HandleFunc("/{id}", UpdateUser).Methods(http.MethodPatch)
-	s.HandleFunc("/{id}", DeleteUser).Methods(http.MethodDelete)
-	s.HandleFunc("/{id}", CreateAccount).Methods(http.MethodPost)
-	//s.HandleFunc("/{id}", DeleteAccount).Methods(http.MethodDelete)
+	s.HandleFunc("/users/{id}", ReadUser).Methods(http.MethodGet)
+	s.HandleFunc("/users/{id}", UpdateUser).Methods(http.MethodPatch)
+	s.HandleFunc("/users/{id}", DeleteUser).Methods(http.MethodDelete)
+
+	s.HandleFunc("/users/{id}", CreateAccount).Methods(http.MethodPost)
+	s.HandleFunc("/users/{id}/{AccountID}", DeleteAccount).Methods(http.MethodDelete)
+
+	s.HandleFunc("/transactions", ListTransactions).Methods(http.MethodGet)
+	s.HandleFunc("/transactions", Transactions).Methods(http.MethodPost)
 
 	http.Handle("/", r)
 	if err := http.ListenAndServe(":9090", nil); err != nil {
@@ -64,37 +80,27 @@ func main() {
 }
 
 func ListUsers(w http.ResponseWriter, r *http.Request) {
-	db := dbConnect()
 	rows, err := db.Query("SELECT * FROM users ORDER BY id DESC")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	employee := Employee{}
+	defer rows.Close()
 	result := []Employee{}
-
 	for rows.Next() {
-		var id int
-		var first_name, last_name, email string
-		err := rows.Scan(&id, &first_name, &last_name, &email)
+		e := new(Employee)
+		err := rows.Scan(&e.ID, &e.FirstName, &e.LastName, &e.Email)
 		if err != nil {
 			log.Fatal(err)
 		}
-		employee.ID = id
-		employee.FirstName = first_name
-		employee.LastName = last_name
-		employee.Email = email
-		result = append(result, employee)
+		result = append(result, *e)
 	}
 	marshaled, _ := json.MarshalIndent(result, "", " ")
 	w.Write(marshaled)
-	defer db.Close()
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	var employee Employee
-	db := dbConnect()
-	if err := json.NewDecoder(r.Body).Decode(&employee); err != nil {
+	var e Employee
+	if err := json.NewDecoder(r.Body).Decode(&e); err != nil {
 		fmt.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "Internal Server Error111")
@@ -102,16 +108,14 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	sqlStatement := "INSERT INTO users (first_name, last_name, email)	VALUES ($1, $2, $3)"
-	_, err := db.Exec(sqlStatement, employee.FirstName, employee.LastName, employee.Email)
+	_, err := db.Exec(sqlStatement, e.FirstName, e.LastName, e.Email)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	http.Redirect(w, r, "http://localhost:9090/users/", 301)
-	defer db.Close()
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	db := dbConnect()
 	id := mux.Vars(r)["id"]
 	if id == "" {
 		w.WriteHeader(http.StatusBadRequest)
@@ -121,38 +125,31 @@ func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	sqlStatement := "DELETE FROM users WHERE id = $1"
 	_, err := db.Exec(sqlStatement, id)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	http.Redirect(w, r, "http://localhost:9090/users/", 301)
-	defer db.Close()
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	var employee Employee
-
-	db := dbConnect()
-
-	if err := json.NewDecoder(r.Body).Decode(&employee); err != nil {
+	var e Employee
+	if err := json.NewDecoder(r.Body).Decode(&e); err != nil {
 		fmt.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "Internal Server Error")
 		return
 	}
 	id := mux.Vars(r)["id"]
-	employee.ID, _ = strconv.Atoi(id)
+	e.ID, _ = strconv.Atoi(id)
 
 	sqlStatement := "UPDATE users SET first_name = $2, last_name = $3, email = $4 WHERE id = $1"
-	_, err := db.Exec(sqlStatement, employee.ID, employee.FirstName, employee.LastName, employee.Email)
+	_, err := db.Exec(sqlStatement, e.ID, e.FirstName, e.LastName, e.Email)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
 	http.Redirect(w, r, "http://localhost:9090/users/", 301)
-	defer db.Close()
 }
 
 func ReadUser(w http.ResponseWriter, r *http.Request) {
-	db := dbConnect()
 	id := mux.Vars(r)["id"]
 	if id == "" {
 		w.WriteHeader(http.StatusBadRequest)
@@ -164,60 +161,44 @@ func ReadUser(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	employee := Employee{}
-
+	defer rows.Close()
+	e := new(Employee)
 	for rows.Next() {
-		var id int
-		var first_name, last_name, email string
-		err = rows.Scan(&id, &first_name, &last_name, &email)
+		err = rows.Scan(&e.ID, &e.FirstName, &e.LastName, &e.Email)
 		if err != nil {
 			log.Fatal(err)
 		}
-		employee.ID = id
-		employee.FirstName = first_name
-		employee.LastName = last_name
-		employee.Email = email
 	}
-	marshaled, _ := json.MarshalIndent(employee, "", " ")
+	marshaled, _ := json.MarshalIndent(e, "", " ")
 	w.Write(marshaled)
-
 	sqlStatement = "SELECT * FROM accounts WHERE id_user = $1"
 	rows, err = db.Query(sqlStatement, id)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	account := Account{}
+	defer rows.Close()
 	result := []Account{}
-
 	for rows.Next() {
-		var id, id_user, value int
-		err = rows.Scan(&id, &id_user, &value)
+		a := new(Account)
+		err = rows.Scan(&a.ID, &a.UserID, &a.Value)
 		if err != nil {
 			log.Fatal(err)
 		}
-		account.ID = id
-		account.ID_User = id_user
-		account.Value = value
-		result = append(result, account)
+		result = append(result, *a)
 	}
 	marshaled, _ = json.MarshalIndent(result, "", " ")
 	w.Write(marshaled)
-	defer db.Close()
 }
 
 func CreateAccount(w http.ResponseWriter, r *http.Request) {
-	db := dbConnect()
 	id := mux.Vars(r)["id"]
 	if id == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "ID mustn't be empty")
 		return
 	}
-	var account Account
-
-	if err := json.NewDecoder(r.Body).Decode(&account); err != nil {
+	var a Account
+	if err := json.NewDecoder(r.Body).Decode(&a); err != nil {
 		fmt.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "Internal Server Error111")
@@ -225,10 +206,116 @@ func CreateAccount(w http.ResponseWriter, r *http.Request) {
 	}
 
 	sqlStatement := "INSERT INTO accounts (id_user, value)	VALUES ($1, $2)"
-	_, err := db.Exec(sqlStatement, id, account.Value)
+	_, err := db.Exec(sqlStatement, id, a.Value)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	http.Redirect(w, r, r.URL.RequestURI(), 301)
-	defer db.Close()
+}
+
+func DeleteAccount(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	account_id := mux.Vars(r)["AccountID"]
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "ID mustn't be empty")
+		return
+	}
+	sqlStatement := "DELETE FROM accounts WHERE id = $1"
+	_, err := db.Exec(sqlStatement, account_id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := fmt.Sprintf("http://localhost:9090/users/%s", id)
+	http.Redirect(w, r, s, 301)
+}
+
+func ListTransactions(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT * FROM transactions ORDER BY id DESC")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	result := []Transaction{}
+	for rows.Next() {
+		t := new(Transaction)
+		err := rows.Scan(&t.ID, &t.Type, &t.AccountForOperationID, &t.RecievedIDAccount, &t.Value)
+		if err != nil {
+			log.Fatal(err)
+		}
+		result = append(result, *t)
+	}
+	marshaled, _ := json.MarshalIndent(result, "", " ")
+	w.Write(marshaled)
+}
+
+func Transactions(w http.ResponseWriter, r *http.Request) {
+	var t Transaction
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Internal Server Error111")
+		return
+	}
+	switch command := t.Type; command {
+	case "WriteOff":
+		sqlStatement := "INSERT INTO transactions (type, account_for_operation_id," +
+			"value)	VALUES ($1, $2, $3)"
+		_, err := db.Exec(sqlStatement, t.Type, t.AccountForOperationID, t.Value)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sqlStatement = "UPDATE accounts SET value = value - $1	WHERE id = $2"
+		_, err = db.Exec(sqlStatement, t.Value, t.AccountForOperationID)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "Refill":
+		sqlStatement := "INSERT INTO transactions (type, account_for_operation_id," +
+			"value)	VALUES ($1, $2, $3)"
+		_, err := db.Exec(sqlStatement, t.Type, t.AccountForOperationID, t.Value)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sqlStatement = "UPDATE accounts SET value = value + $1	WHERE id = $2"
+		_, err = db.Exec(sqlStatement, t.Value, t.AccountForOperationID)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "Transfer":
+		tx, err := db.Begin()
+		if err != nil {
+			log.Fatal(err)
+		}
+		sqlStatement := "INSERT INTO transactions (type, account_for_operation_id," +
+			"recieve_id_account, value)	VALUES ($1, $2, $3, $4)"
+		_, err = db.Exec(sqlStatement, t.Type, t.AccountForOperationID, t.RecievedIDAccount, t.Value)
+		if err != nil {
+			log.Fatal(err)
+		}
+		stmt, err := tx.Prepare("UPDATE accounts SET value = value + $1	WHERE id = $2")
+		if err != nil {
+			tx.Rollback()
+			log.Fatal(err)
+		}
+		defer stmt.Close()
+		if _, err := stmt.Exec(t.Value, t.RecievedIDAccount); err != nil {
+			tx.Rollback()
+			log.Fatal(err)
+		}
+		stmt, err = tx.Prepare("UPDATE accounts SET value = value - $1	WHERE id = $2")
+		if err != nil {
+			tx.Rollback()
+			log.Fatal(err)
+		}
+		defer stmt.Close()
+		if _, err := stmt.Exec(t.Value, t.AccountForOperationID); err != nil {
+			tx.Rollback()
+			log.Fatal(err)
+		}
+		tx.Commit()
+	}
+	http.Redirect(w, r, r.URL.RequestURI(), 301)
 }
